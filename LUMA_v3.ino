525cd5864d9ab9f6349bb772ea0a04a1fa7cbc33
Eben Siyabalapitiya <105612607+Eben-Siyabalapitiya@users.noreply.github.com>
2026-06-25 13:11:35 -0400

    Update LUMA_v3.ino

--- LUMA_v3.ino
+++ LUMA_v3.ino
@@ -50,9 +50,7 @@ Mode mode = FACE;
 unsigned long lastInteraction = 0;
 const unsigned long IDLE_TIMEOUT = 30000;
 
-
-
-// TOUCH PINS
+// TOUCH PINSSS
 #define TOUCH_SWITCH  27
 #define TOUCH_EMOTION 14
 bool lastSwitchTouch = false, lastEmotionTouch = false;
